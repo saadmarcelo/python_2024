a02db6a7253384fe980555d636567eaf6ef1c305
Marcelo saad <marcelo_saad@hotmail.com>
2024-09-05 14:33:58 -0400

    Aula 1 e 2

---
+++ secao_3/aula1.py
@@ -0,0 +1,15 @@
+# Permite escrever um comentario
+
+print("Mostrando na tela")  # frente
+
+
+"""
+    comentarios em bloco
+
+    multi linhas
+
+"""
+
+"""
+mesma coisa de comentario bloco
+"""
---
+++ secao_3/aula2.py
@@ -0,0 +1,4 @@
+print(12, 34, sep="-")
+print(56, 78, sep="*", end="##")
+print(9, 10, sep="*")
+print(11, 12, sep="*")
