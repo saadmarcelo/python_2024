9c70ab84f4cb73e15e27b1f67f3860f80b118edc
Marcelo saad <marcelo_saad@hotmail.com>
2024-09-10 17:58:37 -0400

    secao 4 aula 1 a 2

---
+++ secao_4/aula1.py
@@ -0,0 +1,28 @@
+"""Intrudução as funcoes def em python """
+
+
+def Print():
+    print("Varias")
+
+
+Print()
+
+
+def imprimir(a, b, c):
+    print(a, b, c)
+    print("Varias2")
+    print("Varias3")
+    print("Varias4")
+    print("Varias5")
+
+
+imprimir(1, 2, 3)
+
+
+def saudacao(nome):
+    print(f"Olá, {nome}")
+
+
+saudacao("Marcelo")
+saudacao("Maria")
+saudacao("Laura")
---
+++ secao_4/aula2.py
@@ -0,0 +1,8 @@
+def soma(x, y):
+    # Definicao
+    print(f"{x=} y={y}", "|", "x + y = ", x + y)
+
+
+soma(1, 2)
+soma(2, 1)
+soma(y=2, x=1)
