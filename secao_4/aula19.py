499fdc3900f7ee33c66c9b859593ff8f89f0d332
Marcelo saad <marcelo_saad@hotmail.com>
2024-09-19 11:57:52 -0400

    secao 4 aula 19 e exercicio 20

---
+++ secao_4/aula19.py
@@ -0,0 +1,14 @@
+"""
+Exemplo de uso dos sets
+"""
+
+letras = set()
+while True:
+    letra = input("Digite: ")
+    letras.add(letra.lower())
+
+    if "l" in letras:
+        print("Parabens vc enctrou a letra misteriosa")
+        break
+
+    print(letras)
