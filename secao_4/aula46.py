35dd41fc2f70a01f68f2a76f0466ca087924b780
Marcelo saad <marcelo_saad@hotmail.com>
2024-10-28 10:08:22 -0400

    aula 46 a 48

--- secao_4/aula46.py
+++ secao_4/aula46.py
@@ -15,6 +15,7 @@ def criar_funcao(func):
     return interna
 
 
+@criar_funcao
 def inverte_string(string):
     return string[::-1]
 
@@ -24,6 +25,5 @@ def e_string(param):
         raise TypeError("param deve ser uma string")
 
 
-inverte_string_checando_parametro = criar_funcao(inverte_string)
-invertida = inverte_string_checando_parametro(123)
+invertida = inverte_string("123")
 print(invertida)
---
+++ secao_4/aula47.py
@@ -0,0 +1,23 @@
+"""
+Decoradores com parametros
+"""
+
+
+def decoradora(func):
+    print("Decoradora 1")
+
+    def aninhada(*args, **kwargs):
+        print("Aninhada")
+        res = func(*args, **kwargs)
+        return res
+
+    return aninhada
+
+
+@decoradora
+def soma(x, y):
+    return x + y
+
+
+dez_mais_cinco = soma(10, 5)
+print(dez_mais_cinco)
