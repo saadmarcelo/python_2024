140ebeb1b5309f96ad1342520a2b1ec98856cc0d
Marcelo saad <marcelo_saad@hotmail.com>
2024-12-03 15:20:37 -0400

    count

---
+++ secao_4/aula51.py
@@ -0,0 +1,24 @@
+# count é um iterador sem fim( itertools)
+from itertools import count
+
+c1 = count(10)
+r1 = range(10, 100)
+
+print("c1", hasattr(c1, "__iter__"))
+print("c1", hasattr(c1, "__next__"))
+print("r1", hasattr(r1, "__iter__"))
+print("r1", hasattr(r1, "__next__"))
+print("count")
+for i in c1:
+    if i > 100:
+        break
+
+    print(i)
+
+
+print("range")
+for i in r1:
+    if i > 100:
+        break
+
+    print(i)
